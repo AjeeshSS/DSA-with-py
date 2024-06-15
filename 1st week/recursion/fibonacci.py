0c3c9ee6b986aa1e3f3c317c55f442095d1b8d5a
AjeeshSS <ajeeshss2019@gmail.com>
2024-06-15 13:49:16 +0530

    updated

--- 1st week/recursion/fibonacci.py
+++ 1st week/recursion/fibonacci.py
@@ -1,5 +1,6 @@
 #  fibonacci series of 5 is  0, 1, 1, 2, 3
-
+ 
+# Using recursion
 def fibonacci(n):
     if n <= 1:
         return n
@@ -14,3 +15,44 @@ def print_fibonacci(n):
 
 print(fibonacci(10))  # Output: 55
 print_fibonacci(10)  # Output: 0 1 1 2 3 5 8 13 21 34
+
+
+# using memoization
+memo = [None] * 100   
+counter = 0
+
+def fib (n):
+    global counter
+    counter += 1
+    
+    if memo [n] is not None: 
+        return memo[n]
+    
+    if n == 0 or n == 1:
+        return n
+    
+    memo[n] = fib(n - 1) + fib(n - 2)
+    
+    return memo[n]
+
+n=7 
+
+print("fib of", n, '=',fib(n))
+print("counter: ", counter)
+
+
+# Another example
+counter = 0
+
+def fib (n):
+    fib_list = [0, 1]
+    global counter
+    for index in range(2, n+1):
+        counter += 1
+        next_fib = fib_list[index-1] + fib_list[index-2]
+        fib_list.append(next_fib)
+    return fib_list[n]
+
+n = 7
+print('Fib of', n, '=', fib(n))
+print("counter: ", counter)
